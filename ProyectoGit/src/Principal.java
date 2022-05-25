7ecc637aac6521d74c67e9259e0b76eb1fd8c45e
Alex51z <alex51z@protonmail.com>
2022-05-25 11:03:17 +0200

    Subir proyecto

--- ProyectoGit/src/Principal.java
+++ ProyectoGit/src/Principal.java
@@ -6,10 +6,14 @@ public class Principal {
 		int va=0, ve=0, vi=0, vo=0, vu=0;
 		String [] array= {"ANA", "LUIS", "ANTONIO", "MANUEL"};
 		
+		vocales(array, va, ve, vi, vo ,vu);
+	}
+
+	private static void vocales(String[] array, int va, int ve, int vi, int vo, int vu) {
 		for (int i=0; i<array.length; i++) {
 			for(int r=0; r<array[i].length();r++) {
 				 if (array[i].charAt(r)=='a'){
-					  va++;
+					 va++;  
 				}else {
 					if (array[i].charAt(r)=='e') {
 						ve++;
@@ -29,5 +33,10 @@ public class Principal {
 				}
 			}
 		}
+		System.out.println("Numero de vocales a: "+va);
+		System.out.println("Numero de vocales a: "+ve);
+		System.out.println("Numero de vocales a: "+vi);
+		System.out.println("Numero de vocales a: "+vo);
+		System.out.println("Numero de vocales a: "+vu);
 	}
 }
