dea78e47469c5a54de581d0176016444a7a05b81
Alumno_11 <Alumno_11@DESKTOP-4F77U72>
2022-05-25 10:33:41 +0200

    Subir proyecto numero de vocales

---
+++ ProyectoGit/src/Principal.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Principal {
+
+	public static void main (String [] args) {
+		int va=0, ve=0, vi=0, vo=0, vu=0;
+		String [] array= {"ANA", "LUIS", "ANTONIO", "MANUEL"};
+		
+		for (int i=0; i<array.length; i++) {
+			for(int r=0; r<array[i].length();r++) {
+				 if (array[i].charAt(r)=='a'){
+					  va++;
+				}else {
+					if (array[i].charAt(r)=='e') {
+						ve++;
+					}else {
+						if (array[i].charAt(r)=='i') {
+							vi++;
+						}else {
+							if (array[i].charAt(r)=='o') {
+								vo++;
+							}else {
+								if (array[i].charAt(r)=='u') {
+									vu++;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		System.out.println("Número de vocales a: "+ va+"\n"
+						  +"Número de vocales e: "+ ve+"\n"
+						  +"Número de vocales i: "+ vi+"\n"
+						  +"Número de vocales o: "+ vo+"\n"
+						  +"Número de vocales u: "+ vu+"\n");
+	}
+}
