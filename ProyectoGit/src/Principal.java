2ed75b4007d2bcf80cf12dd5f51cc70c69f04bd4
Alex51z <alex51z@protonmail.com>
2022-05-25 11:06:21 +0200

    Subir JavaDock

--- ProyectoGit/src/Principal.java
+++ ProyectoGit/src/Principal.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 
 public class Principal {
-
+	
+	/**
+	 * 
+	 * @param args
+	 * En este se inicializas las varibles y se llama la metodo vocales
+	 */
 	public static void main (String [] args) {
 		int va=0, ve=0, vi=0, vo=0, vu=0;
 		String [] array= {"ANA", "LUIS", "ANTONIO", "MANUEL"};
@@ -9,6 +14,17 @@ public class Principal {
 		vocales(array, va, ve, vi, vo ,vu);
 	}
 
+	/**
+	 * 
+	 * @param array
+	 * @param va
+	 * @param ve
+	 * @param vi
+	 * @param vo
+	 * @param vu
+	 * Este recorre el array sacado las palabaras y sumando las vocales de cada tipo que hay en el array.
+	 * Al final se imprime el numero total de vocales de cada tipo que hay
+	 */
 	private static void vocales(String[] array, int va, int ve, int vi, int vo, int vu) {
 		for (int i=0; i<array.length; i++) {
 			for(int r=0; r<array[i].length();r++) {
